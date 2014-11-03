cdc2dd528704a21490be8ce4b6ac43eb74b2789e
James Hurliman <james.hurliman@gmail.com>
2014-11-02 23:58:06 -0800

    Minor tweak so I could make a pull request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*written by ajHurliman Nov 2, 2014 for Code Fellows
 this takes stdin and tees it into stdout and a file
 determined at run time as the third command line argument*/
+
 'use strict';
 var fs = require('fs');
 var ws = fs.createWriteStream(process.argv[2]);
