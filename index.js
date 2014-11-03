f66d95b7891cf93ed66908564c1d710c0133bbea
James Hurliman <james.hurliman@gmail.com>
2014-11-02 23:55:01 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*written by ajHurliman Nov 2, 2014 for Code Fellows
+this takes stdin and tees it into stdout and a file
+determined at run time as the third command line argument*/
+'use strict';
+var fs = require('fs');
+var ws = fs.createWriteStream(process.argv[2]);
+
+process.stdin.on('data', function(buf){
+  ws.write(buf);
+  process.stdout.write(buf);
+});
